2441dc986e59c179ced250351b5348ce4aca0ef2
Colin Bloom <colin@>
2017-04-03 13:36:51 -0700

    initial commit

---
+++ src/main/java/Cart.java
@@ -0,0 +1,6 @@
+public class Cart {
+  private int id;
+  private int user_id;
+  private int product_id;
+  private float total;
+}
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,21 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/sales_tracker_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteSalesQuery = "DELETE FROM sales *;";
+      String deleteProductsQuery = "DELETE FROM products *;";
+      con.createQuery(deleteSalesQuery).executeUpdate();
+      con.createQuery(deleteProductsQuery).executeUpdate();
+    }
+  }
+
+}
